afc85ca8041fb8e64727b7a88b8b7c2ff6cce511
jaspreetsingh <jaspreet.android@gmail.com>
2022-11-04 12:08:09 +0530

    removed pods

--- Pods/Target Support Files/Pods-maggnet-maggnetUITests/Pods-maggnet-maggnetUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_maggnet_maggnetUITests : NSObject
-@end
-@implementation PodsDummy_Pods_maggnet_maggnetUITests
-@end
--- Pods/Target Support Files/Pods-maggnetTests/Pods-maggnetTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_maggnetTests : NSObject
-@end
-@implementation PodsDummy_Pods_maggnetTests
-@end
